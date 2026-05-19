ef768124d1bd6ab6ce9114dabe3398fd917eb151
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-19 11:23:02 -0300

    chore: update shareCode to NC9FAGY2T4H

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=773GAVYZL3R&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=NC9FAGY2T4H&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
