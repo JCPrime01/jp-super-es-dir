6ae1cc118d4ac5a72581820cbb9e42fe3a353228
JCPrime01 <vanzokiller@gmail.com>
2026-05-31 12:04:43 -0300

    chore: shareCode QM37GAS1NGI -> T18A8Q9D7VR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=QM37GAS1NGI&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=T18A8Q9D7VR&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
