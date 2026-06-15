688c79e93ed17ab5408f2db582f43595b2b2aab5
JCPrime01 <vanzokiller@gmail.com>
2026-06-15 18:54:31 -0300

    chore: shareCode Y2EDJ3RVS6S -> MHAYJHNA7UX (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=Y2EDJ3RVS6S&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=MHAYJHNA7UX&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
