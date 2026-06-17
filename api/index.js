6357718e887311a40350b26d4b9bc2e981301c49
JCPrime01 <vanzokiller@gmail.com>
2026-06-17 11:19:41 -0300

    chore: shareCode UMCQDQZ2YLT -> HAACPWKSD5E (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=UMCQDQZ2YLT&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=HAACPWKSD5E&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
