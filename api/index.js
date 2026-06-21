1de5bf419767c392c83a5934df846899c81c27a7
JCPrime01 <vanzokiller@gmail.com>
2026-06-21 12:10:25 -0300

    chore: shareCode DFI4NMGZHI0 -> HEHQ2R40H9I (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=DFI4NMGZHI0&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=HEHQ2R40H9I&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
