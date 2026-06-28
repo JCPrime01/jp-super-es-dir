ba07554b0b89e3c004c7807a0c860fe4eb0f542f
JCPrime01 <vanzokiller@gmail.com>
2026-06-28 01:47:54 -0300

    chore: shareCode KBSRIEWK7C9 -> WLGVPSEA7II (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=KBSRIEWK7C9&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=WLGVPSEA7II&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
