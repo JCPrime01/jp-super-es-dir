bce41d9a0bb2b1053fc0aaac314a2d98c3fe1a71
JCPrime01 <vanzokiller@gmail.com>
2026-07-01 10:11:26 -0300

    chore: shareCode HPCZYU18LXC -> TAGJBJVGNCI (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=HPCZYU18LXC&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=TAGJBJVGNCI&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
