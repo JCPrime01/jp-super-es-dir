193dbb249f86b2d8e1b74cef775259c42878fa7a
JCPrime01 <vanzokiller@gmail.com>
2026-07-05 12:26:43 -0300

    chore: shareCode Y8Z5M1M9H0W -> SOP9BFBW5GV (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=Y8Z5M1M9H0W&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=SOP9BFBW5GV&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
