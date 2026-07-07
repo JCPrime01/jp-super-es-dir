c349407d1cefb001d0b729ed480fa01498836546
JCPrime01 <vanzokiller@gmail.com>
2026-07-07 14:16:26 -0300

    chore: shareCode CDIGO929784ZBLYF -> 929784ZBLYF (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=CDIGO929784ZBLYF&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=929784ZBLYF&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
