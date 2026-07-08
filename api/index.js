1207bd94ecea5fdf613ea10d4b52806262df2d73
JCPrime01 <vanzokiller@gmail.com>
2026-07-08 10:12:34 -0300

    chore: shareCode KRTCKCZ3HXR -> GUOP5B88W64 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=KRTCKCZ3HXR&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=GUOP5B88W64&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
