9d8c5939b3092e0f08ee18c5e5b7538dfc18f7d4
JCPrime01 <vanzokiller@gmail.com>
2026-07-09 08:49:04 -0300

    chore: shareCode GUOP5B88W64 -> 5661MS6TTMZ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=GUOP5B88W64&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=5661MS6TTMZ&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
