6fb92ad6966f89e888a9d229c5800a7f4ed45fd6
JCPrime01 <vanzokiller@gmail.com>
2026-07-15 10:39:01 -0300

    chore: shareCode K9J8GER4ZDD -> 6N4GIXT5BX9 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=K9J8GER4ZDD&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=6N4GIXT5BX9&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
