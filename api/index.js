c70d892bd22e873a8f62c5b46094f5931c9c338f
JCPrime01 <vanzokiller@gmail.com>
2026-07-17 12:06:37 -0300

    chore: shareCode ZL2TE27G797 -> ALI0VV4G5VR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=ZL2TE27G797&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=ALI0VV4G5VR&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
