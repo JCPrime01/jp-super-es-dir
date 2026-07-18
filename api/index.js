2a5ca5fc56d50281999a90e9671ad5091d428f7c
JCPrime01 <vanzokiller@gmail.com>
2026-07-18 11:03:13 -0300

    chore: shareCode ALI0VV4G5VR -> 2EDOHDD86T9 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=ALI0VV4G5VR&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=2EDOHDD86T9&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
