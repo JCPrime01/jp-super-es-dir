bb8718c11ac690c4dc9539547bac4dde31329c7d
JCPrime01 <vanzokiller@gmail.com>
2026-07-19 11:50:58 -0300

    chore: shareCode 2EDOHDD86T9 -> YDGOOQ10UZZ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=2EDOHDD86T9&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=YDGOOQ10UZZ&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
