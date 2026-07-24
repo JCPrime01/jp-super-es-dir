e338b4d4e2ffade6725720ebcb97263ccb9a596d
JCPrime01 <vanzokiller@gmail.com>
2026-07-24 09:56:00 -0300

    chore: shareCode VBE6DH8P656 -> 7E7DPN42GPN (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=VBE6DH8P656&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=7E7DPN42GPN&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
