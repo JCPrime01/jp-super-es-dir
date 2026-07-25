056955076b863caea668a6a0e04d13a52144e776
JCPrime01 <vanzokiller@gmail.com>
2026-07-25 10:18:42 -0300

    chore: shareCode 7E7DPN42GPN -> XQ5SSENNWKF (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=7E7DPN42GPN&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=XQ5SSENNWKF&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
