d89c565cc0cfab8a48d5136e42f2f4db2bf5becc
JCPrime01 <vanzokiller@gmail.com>
2026-07-29 14:05:33 -0300

    chore: shareCode R8FQNWTREUH -> Z0XAAHK2MC7 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=R8FQNWTREUH&afp2=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=Z0XAAHK2MC7&afp2=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
